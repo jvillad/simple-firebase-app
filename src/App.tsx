d8efc78c861b63fc4b052648b6dda5708b66cb58
jvillad <jyvillad@gmail.com>
2022-12-27 22:26:37 +1100

    firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <h1>Hello World</h1>;
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 export default App;
