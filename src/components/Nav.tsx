688b02e4956fb578a35244f2dcf99c06fb05abd0
jvillad <jyvillad@gmail.com>
2022-12-27 23:55:16 +1100

    update nav with sign out and createpost

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,45 @@
 import { Link } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
 import { auth } from '../config/firebase';
 
 function Nav() {
   // hook will authomatically update user details
   const [user] = useAuthState(auth);
 
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
+
   return (
     <div>
       <Link to="/"> Home </Link>
-      <Link to="/login"> Login </Link>
-      <div>
-        <p>{user?.displayName}</p>
-        <img
-          src={user?.photoURL || ''}
-          alt="user media"
-          width={200}
-          height={200}
-        />
+      {!user ? (
+        <Link to="/login"> Login </Link>
+      ) : (
+        <Link to="/createpost"> Create Post </Link>
+      )}
+
+      <div className="user">
+        {/* if user exist display name and photo */}
+        {user && (
+          <>
+            <p>{user?.displayName}</p>
+            <img
+              src={user?.photoURL || ''}
+              alt="user media"
+              width={200}
+              height={200}
+            />
+          </>
+        )}
       </div>
+      {user && (
+        <button type="button" onClick={signUserOut}>
+          {' '}
+          Log Out{' '}
+        </button>
+      )}
     </div>
   );
 }
