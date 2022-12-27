1e926be0e026a9e87f5d49b7074f80e5e1052f38
jvillad <jyvillad@gmail.com>
2022-12-27 22:31:03 +1100

    added login & display current user once logged in

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,24 @@
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../config/firebase';
 
 function Nav() {
+  // hook will authomatically update user details
+  const [user] = useAuthState(auth);
+
   return (
     <div>
       <Link to="/"> Home </Link>
       <Link to="/login"> Login </Link>
+      <div>
+        <p>{user?.displayName}</p>
+        <img
+          src={user?.photoURL || ''}
+          alt="user media"
+          width={200}
+          height={200}
+        />
+      </div>
     </div>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,21 @@
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { auth, provider } from '../config/firebase';
+
 function Login() {
-  return <h1>Login</h1>;
+  const navigate = useNavigate();
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    navigate('/');
+  };
+  return (
+    <div>
+      <p>Sign in with google to continue. </p>
+      <button type="button" onClick={signInWithGoogle}>
+        {' '}
+        Log In{' '}
+      </button>
+    </div>
+  );
 }
 export default Login;
