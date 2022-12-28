7a214e8ad9afb999b2012fc3f7d8dbb513783863
jvillad <jyvillad@gmail.com>
2022-12-28 23:37:54 +1100

    getPosts from dbStore

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,35 @@
+import { getDocs, collection } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { dbStore } from '../config/firebase';
+import Post from './Post';
+
+interface UserPost {
+  id: string;
+  userId: string;
+  title: string;
+  username: string;
+  descriptiong: string;
+}
+
 function Home() {
-  return <h1>Home</h1>;
+  const [postsList, setPostsList] = useState<UserPost[] | null>(null);
+  // fetch to dbStore using firebase function
+  const postsRef = collection(dbStore, 'posts');
+  const fetchPosts = async () => {
+    const data = await getDocs(postsRef);
+    setPostsList(
+      data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as UserPost[]
+    );
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      {postsList?.map((post) => (
+        <Post key="key" />
+      ))}
+    </div>
+  );
 }
 export default Home;
