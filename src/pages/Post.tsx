af0d9e16d50c4cbb5642b11a49455cdbcb794519
jvillad <jyvillad@gmail.com>
2022-12-29 12:32:02 +1100

    added a like function, display number of likes

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,12 +1,52 @@
 /* eslint-disable import/no-cycle */
+import { addDoc, getDocs, collection, query, where } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRevalidator } from 'react-router-dom';
+import { auth, dbStore } from '../config/firebase';
 import { UserPost as IPost } from './Home';
 
 interface Props {
   post: IPost;
 }
 
+interface Like {
+  userId: string;
+}
+
 function Post(props: Props) {
   const { post } = props;
+  const [likes, setLikes] = useState<Like[] | null>(null);
+  const [user] = useAuthState(auth);
+  const likesRef = collection(dbStore, 'likes');
+  const likesDoc = query(likesRef, where('postId', '==', post.id));
+  // TODO: separate func to easily track errors/debug
+  const getLikes = async () => {
+    const data = await getDocs(likesDoc);
+    setLikes(data.docs.map((doc) => ({ userId: doc.data().userId })));
+  };
+
+  // TODO make a new function
+  // get user who like the post
+  const addLike = async () => {
+    try {
+      await addDoc(likesRef, { userId: user?.uid, postId: post.id });
+      if (user) {
+        setLikes((prev) =>
+          prev ? [...prev, { userId: user.uid }] : [{ userId: user.uid }]
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const hasUserLiked = likes?.find((like) => like.userId === user?.uid);
+
+  useEffect(() => {
+    getLikes();
+  }, []);
+
   return (
     <div>
       <div className="title">
@@ -15,9 +55,12 @@ function Post(props: Props) {
       <div className="body">
         <p>{post.description}</p>
       </div>
-
       <div className="footer-post">
         <p>@{post.username}</p>
+        <button type="button" onClick={addLike}>
+          {hasUserLiked ? <>&#128078;</> : <>&#128077;</>}
+        </button>
+        {likes && <p>Likes: {likes.length}</p>}
       </div>
     </div>
   );
