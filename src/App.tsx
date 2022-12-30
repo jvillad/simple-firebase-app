3ac71a2b706215efe5f5093cde4425fda08e3e10
jvillad <jyvillad@gmail.com>
2022-12-30 13:21:39 +1100

    update import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav';
+import Nav from './nav/Nav';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import CreatePost from './pages/CreatePost/CreatePost';
